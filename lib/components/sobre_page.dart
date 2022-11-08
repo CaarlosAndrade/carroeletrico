fd8ee8accecb5fb7a51cd30bb1a939dd93be9537
Carlos Rocha <carlosandrade399@gmail.com>
2022-11-08 20:41:10 -0300

    feat: implementado stepper

--- lib/components/sobre_page.dart
+++ lib/components/sobre_page.dart
@@ -1,36 +1,69 @@
 import 'package:flutter/material.dart';
 
 class SobrePage extends StatefulWidget {
-  const SobrePage({Key? key}) : super(key: key);
+  const SobrePage({super.key});
 
   @override
   State<SobrePage> createState() => _SobrePageState();
 }
 
 class _SobrePageState extends State<SobrePage> {
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  int _index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sobre'),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(children: [
-            Text("Henrique Teixeira Conti"),
-            const SizedBox(height: 20),
-            Text("Carlos Andrade Rocha")
-          ]),
-        ),
-      ),
-    );
+        appBar: AppBar(
+            title: const Text('Stellantis'), automaticallyImplyLeading: false),
+        body: Column(
+          children: [
+            Stepper(
+              controlsBuilder: (BuildContext context, ControlsDetails details) {
+                return Row(
+                  children: <Widget>[
+                    TextButton(
+                      onPressed: () => {},
+                      child: const Text(''),
+                    ),
+                    TextButton(
+                      onPressed: () => {},
+                      child: const Text(''),
+                    ),
+                  ],
+                );
+              },
+              currentStep: _index,
+              onStepContinue: () {
+                if (_index <= 0) {
+                  setState(() {
+                    _index += 1;
+                  });
+                }
+              },
+              onStepTapped: (int index) {
+                setState(() {
+                  _index = index;
+                });
+              },
+              steps: <Step>[
+                Step(
+                  isActive: _index == 0,
+                  title: const Text('Fundação'),
+                  content: Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text('Content for Step 1')),
+                ),
+                Step(
+                  isActive: _index == 1,
+                  title: Text('Step 2 title'),
+                  content: Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text('Content for Step 1')),
+                ),
+              ],
+            ),
+            Image(image: image)
+          ],
+        ));
   }
 }
-
